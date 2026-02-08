9bf6e72bd74049f764bd15e676e91c94fccf8ed7
lszoszk <lszoszk@gmail.com>
2026-02-08 17:32:41 +0100

    Trim repo, ignore scraper files, and fix paragraph wrapping

--- mysite/static/js/cookies.js
+++
@@ -1,2 +0,0 @@
-// This code can be placed in your global script file or a dedicated cookies.js file.
-document.cookie = "myCookieName=myCookieValue; domain=lszoszk.pythonanywhere.com; path=/; Secure; SameSite=Lax";
